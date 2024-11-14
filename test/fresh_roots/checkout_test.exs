a8e55ced9b63739278f4f7577499aebd014a8e81
Riadh <188395871+riadh-ex@users.noreply.github.com>
2024-11-14 22:36:36 +0100

    chore: Add tests for creating an empty cart
    Not supposed to pass for now as the implementation is not done yet.

--- test/fresh_roots/checkout_test.exs
+++ test/fresh_roots/checkout_test.exs
@@ -8,4 +8,10 @@ defmodule FreshRoots.CheckoutTest do
       assert Checkout.list_products() |> length() == 3
     end
   end
+
+  describe "new_cart/0" do
+    test "returns an empty cart" do
+      assert %Checkout.Cart{items: []} = Checkout.new_cart()
+    end
+  end
 end
